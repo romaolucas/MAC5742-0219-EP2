e8f777bd3cba9fd14f4e104c5cf71d921ec4dac6
Isabella Mendonça <isaisaisa@live.dk>
2017-05-30 19:46:26 -0300

    fixing rot13

--- src/crypto-algorithms/rot-13_test.c
+++ src/crypto-algorithms/rot-13_test.c
@@ -17,9 +17,9 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
 #include "rot-13.h"
 #include "util.c"
-#define BYTE unsigned char
 
 /*********************** FUNCTION DEFINITIONS ***********************/
 int rot13_test()
@@ -43,11 +43,13 @@ int rot13_test()
 
 void dec_file(char *enc_filename, BYTE *data) {
     BYTE *enc_data;
+    struct stat st;
     enc_data = (BYTE *) malloc(sizeof(BYTE) * st.st_size);
     strcpy(enc_data, data);
     rot13(enc_data);
     FILE *enc_file = fopen(enc_filename, "wb+");
     fwrite(enc_data, sizeof(BYTE) * st.st_size, 1, enc_file);
+    free(enc_data);
     fclose(enc_file);
 }
 
@@ -62,10 +64,9 @@ int main(int argc, char *argv[])
         printf("./rot-13 nome_arquivo nome_arquivo_criptografado\n");
         exit(EXIT_FAILURE);
     }
-    
+
     data = read_file(argv[1]);
     dec_file(argv[2], data);
     free(data);
-    free(enc_data);
     return(0);
 }
