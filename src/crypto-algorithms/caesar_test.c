c4f8cad2cf9df75a5a865810791dfbdd34fbae52
Isabella Mendonça <isaisaisa@live.dk>
2017-06-08 23:12:45 -0300

    making few changes

--- src/crypto-algorithms/caesar_test.c
+++ src/crypto-algorithms/caesar_test.c
@@ -12,33 +12,15 @@ int main(int argc, char *argv[])
 {
     //printf("ROT-13 tests: %s\n", rot13_test() ? "SUCCEEDED" : "FAILED");
     BYTE *data;
-    BYTE *enc_data;
     struct stat st;
 
-    if (argc != 3) {
-        printf("Uso: ./caesar nome_arquivo nome_arquivo_criptografado\n");
+    if (argc != 2) {
+        printf("Uso: ./caesar nome_arquivo\n");
         exit(EXIT_FAILURE);
     }
 
     data = read_file(argv[1]);
-
-    enc_data = (BYTE *) malloc(sizeof(BYTE) * st.st_size);
-    strcpy(enc_data, data);
-    caesar(13, enc_data);
+    caesar(13, data);
     free(data);
     return(0);
 }
-
-
-// int main()
-// {
-// 	char str[] = "This is a top secret text message!";
- 
-// 	printf("Original: %s\n", str);
-// 	caesar(str);
-// 	printf("Encrypted: %s\n", str);
-// 	decaesar(str);
-// 	printf("Decrypted: %s\n", str);
- 
-// 	return 0;
-// }
