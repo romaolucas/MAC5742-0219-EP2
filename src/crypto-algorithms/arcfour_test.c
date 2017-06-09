69f58a35b1ee5be6476276f989d8834f50bf906c
Isabella Mendonça <isaisaisa@live.dk>
2017-06-09 00:40:38 -0300

    trying to input into arcfour

--- src/crypto-algorithms/arcfour_test.c
+++ src/crypto-algorithms/arcfour_test.c
@@ -59,9 +59,8 @@ int main(int argc, char *argv[])
 
     data = read_file(argv[1]);
     arcfour_key_setup(state, data, strlen(data));
-    arcfour_generate_stream(state, out, 1);
-    printf("%s\n", out);
-    //dec_file(data);
+    //arcfour_generate_stream(state, out, 1);
+    printf("%s\n", data);
     free(data);
     free(out);
 
