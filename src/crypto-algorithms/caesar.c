8444c57dec978e4d9e1b07c4aaa3d96d404aff6e
Isabella Mendonça <isaisaisa@live.dk>
2017-06-08 21:45:56 -0300

    trying to read from input

--- src/crypto-algorithms/caesar.c
+++ src/crypto-algorithms/caesar.c
@@ -1,12 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
- 
-#define caesar(x) rot(13, x)
-#define decaesar(x) rot(13, x)
-#define decrypt_rot(x, y) rot((26-x), y)
- 
-void rot(int c, char *str)
+#include "caesar.h"
+
+void caesar(int c, char str[])
 {
 	int l = strlen(str);
 	const char *alpha[2] = { "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
@@ -19,18 +13,4 @@ void rot(int c, char *str)
  
 		str[i] = alpha[isupper(str[i])][((int)(tolower(str[i])-'a')+c)%26];
 	}
-}
- 
- 
-int main()
-{
-	char str[] = "This is a top secret text message!";
- 
-	printf("Original: %s\n", str);
-	caesar(str);
-	printf("Encrypted: %s\n", str);
-	decaesar(str);
-	printf("Decrypted: %s\n", str);
- 
-	return 0;
 }
---
+++ src/crypto-algorithms/caesar_test.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "caesar.h"
+#include "util.h"
+
+
+int main(int argc, char *argv[])
+{
+    //printf("ROT-13 tests: %s\n", rot13_test() ? "SUCCEEDED" : "FAILED");
+    BYTE *data;
+
+    if (argc != 3) {
+        printf("Uso: ./caesar nome_arquivo nome_arquivo_criptografado\n");
+        exit(EXIT_FAILURE);
+    }
+
+    data = read_file(argv[1]);
+    caesar(data);
+    free(data);
+    return(0);
+}
+
+
+// int main()
+// {
+// 	char str[] = "This is a top secret text message!";
+ 
+// 	printf("Original: %s\n", str);
+// 	caesar(str);
+// 	printf("Encrypted: %s\n", str);
+// 	decaesar(str);
+// 	printf("Decrypted: %s\n", str);
+ 
+// 	return 0;
+// }
