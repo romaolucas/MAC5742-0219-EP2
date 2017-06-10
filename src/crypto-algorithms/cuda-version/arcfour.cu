e2d6cc3c1c0ffb1a9b41edf68fa80763820746e9
Isabella Mendonça <isaisaisa@live.dk>
2017-06-10 14:17:55 -0300

    trying to fix type errors 2

--- src/crypto-algorithms/cuda-version/arcfour.cu
+++ src/crypto-algorithms/cuda-version/arcfour.cu
@@ -59,7 +59,7 @@ __device__ void generate_key(BYTE *generated_key)
     int idx; 
 
     for (idx = 0; idx < 3; idx++)
-        arcfour_key_setup((const char*) state, key[idx], strlen(key[idx]));
+        arcfour_key_setup(state, key[idx], strlen(key[idx]));
     
     arcfour_generate_stream(state, generated_key, strlen(state));
 }
