c09edc481baf794ae23850ec84bafb11f27c6839
Lucas Romao <lucas.romao.silva@gmail.com>
2017-06-11 15:18:37 -0300

    ops

--- src/crypto-algorithms/cuda-version/run_measurements.sh
+++ src/crypto-algorithms/cuda-version/run_measurements.sh
@@ -4,7 +4,7 @@ set -o xtrace
 
 MEASUREMENTS=10
 NAMES=('arcfour' 'des' 'rot-13')
-FILE_NAMES=('../sample_files/moby_dick.txt', '../sample_files/king_james_bible.txt', '../sample_files/hubble_1.tif', '../sample_files/mercury.png')
+FILE_NAMES=('../sample_files/moby_dick.txt' '../sample_files/king_james_bible.txt' '../sample_files/hubble_1.tif' '../sample_files/mercury.png')
 
 make
 mkdir results
