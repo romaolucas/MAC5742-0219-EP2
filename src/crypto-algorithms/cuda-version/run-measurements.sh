f012408b8b58cd1d6a0d93d9046cc6697041a709
Lucas Romao <lucas.romao.silva@gmail.com>
2017-06-11 02:55:46 -0300

    adding script for cuda

---
+++ src/crypto-algorithms/cuda-version/run-measurements.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -o xtrace
+
+MEASUREMENTS=10
+NAMES=('arcfour', 'des', 'rot-13')
+FILE_NAMES=('../sample_files/moby_dick.txt', '../sample_files/king_james_bible.txt', '../sample_files/hubble_1.tif', '../sample_files/mercury.png')
+
+make
+mkdir results
+
+for NAME in ${NAMES[@]}; do
+    for FILE in ${FILE_NAMES[@]}; do
+        perf stat -r $MEASUREMENTS ./$NAME -tf  $FILE >> $FILE.log 2>&1
+    done
+done
