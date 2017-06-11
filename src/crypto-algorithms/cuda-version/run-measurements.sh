c5be3847c287ddcf14c40ea836c8443b1ad0ae77
Lucas Romao <lucas.romao.silva@gmail.com>
2017-06-11 11:06:00 -0300

    fixing script

--- src/crypto-algorithms/cuda-version/run-measurements.sh
+++ src/crypto-algorithms/cuda-version/run-measurements.sh
@@ -3,7 +3,7 @@
 set -o xtrace
 
 MEASUREMENTS=10
-NAMES=('arcfour', 'des', 'rot-13')
+NAMES=('arcfour' 'des' 'rot-13')
 FILE_NAMES=('../sample_files/moby_dick.txt', '../sample_files/king_james_bible.txt', '../sample_files/hubble_1.tif', '../sample_files/mercury.png')
 
 make
@@ -11,6 +11,7 @@ mkdir results
 
 for NAME in ${NAMES[@]}; do
     for FILE in ${FILE_NAMES[@]}; do
-        perf stat -r $MEASUREMENTS ./$NAME -tf  $FILE >> $FILE.log 2>&1
+        perf stat -r $MEASUREMENTS ./$NAME '-tf'  $FILE >> $NAME.log 2>&1
     done
+    mv *.log results/
 done
--- src/crypto-algorithms/run_measurements.sh
+++ src/crypto-algorithms/run_measurements.sh
@@ -3,14 +3,15 @@
 set -o xtrace
 
 MEASUREMENTS=10
-NAMES=('arcfour', 'des', 'rot-13')
-FILE_NAMES=('sample_files/moby_dick.txt', 'sample_files/king_james_bible.txt', 'sample_files/hubble_1.tif', 'sample_files/mercury.png')
+NAMES=('arcfour' 'des' 'rot-13')
+FILE_NAMES=('sample_files/moby_dick.txt' 'sample_files/king_james_bible.txt' 'sample_files/hubble_1.tif' 'sample_files/mercury.png')
 
 make
 mkdir results
 
 for NAME in ${NAMES[@]}; do
     for FILE in ${FILE_NAMES[@]}; do
-        perf stat -r $MEASUREMENTS ./$NAME -tf  $FILE >> $FILE.log 2>&1
+        perf stat -r $MEASUREMENTS ./$NAME '-tf' $FILE  >> $NAME.log 2>&1
     done
+    mv *.log results/
 done
